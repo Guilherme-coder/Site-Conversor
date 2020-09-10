02d752503a5cb7d9576d2b83736763fa1ce0f362
Guilherme Hendres <ghendressmelo@gmail.com>
2020-09-10 16:38:20 -0300

    Finalizando implementações

--- js/index.js
+++ js/index.js
@@ -62,7 +62,27 @@ let campoDecHex = document.querySelector("#dec-hex");
 let labDecHex = document.querySelector("#dec-hex--lab");
 botaoDecHex.addEventListener("click", function(event){
     event.preventDefault;
-    labDecHex.value = campoDecHex.value;
+
+    let numero = campoDecHex.value;
+
+    let daVez;
+    let total = "";
+
+    while (numero >= 16){
+        daVez = numeroParaLetra(numero % 16);
+        numero = parseInt(numero / 16);
+        
+        total = daVez + total;
+    }
+
+    if (numero != 0){
+        numero *= 1;
+        numero = numeroParaLetra(numero);
+        total = numero + total;
+    }
+
+    labDecHex.value = total;
+
 });
 
 let botaoHexDec = document.querySelector("#hex-dec--submit");
@@ -70,5 +90,85 @@ let campoHexDec = document.querySelector("#hex-dec");
 let labHexDec = document.querySelector("#hex-dec--lab");
 botaoHexDec.addEventListener("click", function(event){
     event.preventDefault;
-    labHexDec.value = campoHexDec.value;
+
+    let numero = campoHexDec.value;
+    let indice = numero.length - 1;
+    let daVez;
+    let total = 0;
+    let multiplicador = 0;
+
+    while(indice >= 0){
+        daVez = letraParaNumero(numero[indice]);
+        daVez *= Math.pow(16, multiplicador);
+        
+        
+        
+        
+        total = daVez + total;
+        indice -= 1;
+        multiplicador += 1;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    labHexDec.value = total;
+    
 });
+
+
+
+function numeroParaLetra(numero){
+    switch(numero){
+        case 10:
+            numero = "A";
+            break;
+        case 11: 
+            numero = "B";
+            break;
+        case 12:
+            numero = "C";
+            break; 
+        case 13:
+            numero = "D";
+            break;
+        case 14:
+            numero = "E";
+            break;
+        case 15:
+            numero = "F";
+            break;
+    };
+    return numero;
+};
+
+
+function letraParaNumero(letra){
+    let letraGrande = letra.toUpperCase()
+    switch(letraGrande){
+        case "A":
+            letraGrande = 10;
+            break;
+        case "B": 
+            letraGrande = 11;
+            break;
+        case "C":
+            letraGrande = 12;
+            break; 
+        case "D":
+            letraGrande = 13;
+            break;
+        case "E":
+            letraGrande = 14;
+            break;
+        case "F":
+            letraGrande = 15;
+            break;
+    };
+    return letraGrande;
+};
